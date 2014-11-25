229099788a1ad1d78ba58b06138cabc434b8249a
guilhermeleobas <guilhermeleobas@gmail.com>
2014-11-25 16:39:42 -0200

    update mkdir.c

--- mkdir.c
+++ mkdir.c
@@ -18,6 +18,51 @@
 #define notvalid -1
 #define success 0
 
+/*
+void inc_folder_size (struct superblock *sb, uint64_t father_nodeinfo_block, const char *dname){
+
+	struct nodeinfo *n_info = (struct nodeinfo*) malloc (sb->blksz);
+	SEEK_READ (sb, father_nodeinfo_block, n_info);
+
+	n_info->size++;
+
+	SEEK_WRITE (sb, father_nodeinfo_block, n_info);
+
+}
+*/
+
+void init_folder_struct (struct inode* folder, uint64_t folder_block, uint64_t nodeinfo_block) {
+
+	 /* inode properties for a folder */ 
+
+  /* =mode indicates that the inode is a directory */
+  folder->mode = IMDIR;
+  /* =parent points to the block that contains this inode */
+  folder->parent = folder_block;
+  /* start the =next value with 0 */
+  folder->next = 0;
+  /* =meta value must point to the nodeinfo block */
+  folder->meta = nodeinfo_block;
+
+}
+
+void init_nodeinfo_struct (const char* dname, struct nodeinfo* n_info, uint64_t nodeinfo_block){
+	/* nodeinfo properties */
+
+  /* =size contains the number of files in the directory*/
+  n_info->size = 0;
+  /* =name contains the name of the new folder
+   * i.e. /home/user/dir1/folder
+	 * =name is equals to folder
+   */
+	int size = 0;
+	char** dname_array = getFileParts (dname, &size);
+	strcpy (n_info->name, dname_array[size-1]);
+  freeFileParts (&dname_array, size);
+
+}
+
+
 int fs_mkdir(struct superblock *sb, const char *dname){
 
   if (!sb->freeblks){
@@ -37,43 +82,28 @@ int fs_mkdir(struct superblock *sb, const char *dname){
   }
 
   struct inode *father = (struct inode*) malloc (sb->blksz);
-  struct inone *folder = (struct inode*) malloc (sb->blksz);
+  struct inode *folder = (struct inode*) malloc (sb->blksz);
   struct nodeinfo *n_info = (struct nodeinfo*) malloc (sb->blksz);
 
   uint64_t folder_block = fs_get_block (sb);
   uint64_t nodeinfo_block = fs_get_block (sb);
 
-
-  /* inode properties for a folder */ 
-
-  /* =mode indicates that the inode is a directory */
-  folder->mode = IMDIR;
-  /* =parent points to the block that contains this inode */
-  folder->parent = folder_block;
-  /* start the =next value with 0 */
-  folder->next = 0;
-  /* =meta value must point to the nodeinfo block */
-  folder->meta = nodeinf_block;
-
-
-  /* nodeinfo properties */
-
-  /* =size contains the number of files in the directory*/
-  n_info->size = 0;
-  /* =name contains the full path to the directory
-   * i.e. /home/user/dir1/folder
-   */
-  n_info->name = dname;
-
-
+	init_folder_struct (folder, folder_block, nodeinfo_block);
+	init_nodeinfo_struct (dname, n_info, nodeinfo_block);
+ 
   if (!exists){
-    /* Read the inode stored in file */
-    SEEK_READ (sb, fileBlock, father);
+    /* Read father inode stored in file */
+    // SEEK_READ (sb, fileBlock, father);
 
-    
+		addFileToDirBlock (sb, fileBlock, folder_block);
 
+		/* store both inodes related to the folder that
+			 has been just created
+		*/
     SEEK_WRITE (sb, folder_block, folder);
     SEEK_WRITE (sb, nodeinfo_block, n_info);
     
   }
+
+	return success;
 }
