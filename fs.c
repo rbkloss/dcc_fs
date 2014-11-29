02fcc2726f61f021e3a3d268af303716feafbc80
rbk <klossricardo@gmail.com>
2014-11-29 12:59:15 -0200

    merge

--- fs.c
+++ fs.c
@@ -380,6 +380,9 @@ ssize_t fs_read_file(struct superblock *sb, const char *fname, char *buf,
     return size;
 }
 
+/*
+ * Francisco:
+ */
 int fs_delete_file(struct superblock *sb, const char *fname) { //o proprio nome ja diz.
     int i = 0, found, maxLink, ultimo;
     uint64_t fileBlock, fileLinkBlk, lastBlock, folderBlock; //numero do bloco e do bloco do link no diretorio que tem ele
@@ -530,6 +533,104 @@ char * fs_list_dir(struct superblock *sb, const char *dname) { //lista tudo o qu
     free(auxinfo);
     printf("%s\n", names);
     return names;
+}
+/*
+ * Leobas:
+ */
+#define notvalid -1
+#define success 0
+
+/*
+void inc_folder_size (struct superblock *sb, uint64_t father_nodeinfo_block, const char *dname){
+
+        struct nodeinfo *n_info = (struct nodeinfo*) malloc (sb->blksz);
+        SEEK_READ (sb, father_nodeinfo_block, n_info);
+
+        n_info->size++;
+
+        SEEK_WRITE (sb, father_nodeinfo_block, n_info);
+
+}
+ */
+
+void init_folder_struct(struct inode* folder, uint64_t folder_block, uint64_t nodeinfo_block) {
+
+    /* inode properties for a folder */
+
+    /* =mode indicates that the inode is a directory */
+    folder->mode = IMDIR;
+    /* =parent points to the block that contains this inode */
+    folder->parent = folder_block;
+    /* start the =next value with 0 */
+    folder->next = 0;
+    /* =meta value must point to the nodeinfo block */
+    folder->meta = nodeinfo_block;
+
+}
+
+void init_nodeinfo_struct(const char* dname, struct nodeinfo* n_info, uint64_t nodeinfo_block) {
+    /* nodeinfo properties */
+
+    /* =size contains the number of files in the directory*/
+    n_info->size = 0;
+    /* =name contains the name of the new folder
+     * i.e. /home/user/dir1/folder
+     * =name is equals to folder
+     */
+    int size = 0;
+    char** dname_array = getFileParts(dname, &size);
+    strcpy(n_info->name, dname_array[size - 1]);
+    freeFileParts(&dname_array, size);
+
+}
+
+int fs_mkdir(struct superblock *sb, const char *dname) {
+
+    if (!sb->freeblks) {
+        // disk is full
+        errno = ENOSPC;
+        return notvalid;
+    }
+
+    int exists = 0;
+
+    uint64_t fileBlock = findFile(sb, dname, &exists);
+
+    if (exists) {
+        // dir already exist;
+        errno = EEXIST;
+        return notvalid;
+    }
+
+    struct inode *father , *folder;
+    initNode(&father, sb->blksz);
+    initNode(&folder, sb->blksz);
+    struct nodeinfo *n_info = (struct nodeinfo*) calloc(1, sb->blksz);
+
+    uint64_t folder_block = fs_get_block(sb);
+    uint64_t nodeinfo_block = fs_get_block(sb);
+
+    init_folder_struct(folder, folder_block, nodeinfo_block);
+    init_nodeinfo_struct(dname, n_info, nodeinfo_block);
+
+    if (!exists) {
+        /* Read father inode stored in file */
+        // SEEK_READ (sb, fileBlock, father);
+
+        insertInBlock(sb, fileBlock, folder_block);
+
+        /* store both inodes related to the folder that
+                 has been just created
+         */
+        seek_write(sb, folder_block, folder);
+        seek_write(sb, nodeinfo_block, n_info);
+
+    }
+    free(folder);
+    free(father);
+    free(n_info);
+
+    return success;
 }
 
 //int fs_delete_file(struct superblock *sb, const char *fname) { //o proprio nome ja diz.
